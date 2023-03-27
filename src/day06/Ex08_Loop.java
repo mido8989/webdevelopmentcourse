f835c8240692ad74b1b8a8359f4dbc73ac12f020
DESKTOP-KHVJGSC\user <0414soyeon@gmail.com>
2023-03-27 21:46:38 +0900

    day6

--- src/day06/Ex08_Loop.java
+++ src/day06/Ex08_Loop.java
@@ -1,5 +1,7 @@
 package day06;
 
+import java.util.Random;
+
 import util.MyUtil;
 
 public class Ex08_Loop {
@@ -68,8 +70,50 @@ public class Ex08_Loop {
 			u.p("매우 열심히 공부해야겠네");
 			m++;
 		} while(false);
-	}
+		
+		// 4. break
+		// while과 break를 사용하여
+		// 1에서 100까지를 출력하되
+		// 3의 배수는 빼고 출력하는 로직을 작성해보세요.
+		
+		int n = 1;
+		while (true) {
+			if (n % 3 !=0) {
+				u.p("[3의배수제외]" + n);
+			}
+			n++;
+			if (n > 100) {
+				break;
 
+		}
+		
+	}
+		
+		// 5. continue 
+		while(true) {
+			Random rd = new Random();
+			boolean server_open = false;
+			if(rd.nextInt(2)==1) {
+				server_open = true;
+			}
+			
+			if(!server_open) {
+				// 3초 휴식
+				u.p("[서버close] 3초 휴식합니다 ");
+
+				try {
+					Thread.sleep(3000);
+				} catch(Exception e) {}
+				continue;
+			}
+			u.p("[서버open] 서버에 접속하였습니다");
+			u.p("[서버open] 월드에 입장하셨습니다");
+			u.p("[서버open] 캐릭터 입장하셨습니다");
+			u.p("[서버open] 게임을 플레이합니다. ");
+			break;
+		}
+
+	}
 }
 
 
