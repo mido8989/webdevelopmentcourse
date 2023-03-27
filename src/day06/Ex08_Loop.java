d84a59454b6e37ceb974955f15c61a10d05d2e4a
DESKTOP-KHVJGSC\user <0414soyeon@gmail.com>
2023-03-27 19:53:56 +0900

    day06

--- src/day06/Ex08_Loop.java
+++ src/day06/Ex08_Loop.java
@@ -1,10 +1,48 @@
 package day06;
 
+import util.MyUtil;
+
 public class Ex08_Loop {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		MyUtil u = new MyUtil();
+		
+		// 1. while 
+		u.p("1. while");
+		
+		int i = 0;
+		while(i < 100){
+			u.p("이번 i는 " + i + "입니다");
+			i++;
+	}
+		
+		 // Infinity Loop
+		i = 0;
+		while (true) {
+			// i가 9999일 때 굵은 글자로 표현한다.
+			if(i == 9999) {
+				u.p(u.BOLD + "이번 i는 " + i + "입니다." + u.END);
+			}
+			else {
+				u.p("이번 i는 " + i + "입니다.") ;
+			}
+			i++;
+			if (i >= 10000) {
+				break;
+			}
+		}
+		
 	}
 
 }
+
+
+
+
+
+
+
+
+
+
