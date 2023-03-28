1f0c06d658a4fa54f20243f546bff168a429aa11
DESKTOP-KHVJGSC\user <0414soyeon@gmail.com>
2023-03-28 21:41:35 +0900

    day07

--- src/day07/Ex09_Array.java
+++ src/day07/Ex09_Array.java
@@ -1,5 +1,7 @@
 package day07;
 
+import java.util.Random;
+
 import util.MyUtil;
 
 public class Ex09_Array {
@@ -29,16 +31,80 @@ public class Ex09_Array {
 		u.p("Student[ " + student_name2[3] + "] : " + score_java[3] + "점");
 		u.p("Student[ " + student_name2[4] + "] : " + score_java[4] + "점");
 		
-		
 		u.p("");
 		u.p("Automation");
 		for(int i=0;i<student_name2.length;i++) {
 			u.p("Student[ " + student_name2[i] + "] : " + score_java[i] + "점");
 		}
-
-
 		
+		// 2-dimesnional array
+		String[][] student_with_class = {{"A", "B"}, {"C", "D"}};
+		// A학생
+		u.p(student_with_class[0][0]);
+		// c학생
+		u.p(student_with_class[1][0]);
+		
+		// 그럼 이것은?
+		System.out.println(student_with_class[1]);
+		String[] students = student_with_class[1];
+		u.p("이것이 나옵니까? " + students[0]);
+		
+		String[][] characters = {{"hero", "sword", "blue"} 
+								 ,{"dragon", "fire", "red"}};
+		
+		String[] character = characters[1];
+		u.p("Character Name " + character[0]);
+		
+		// Work 
+		// int 배열인 worker가 있음
+		// Worker의 개수는 random(1~100)으로 정한다.
+		// 각 worker의 값은 random으로 넣으시오.
+		// 전체 worker의 평균을 구하고, "worker 개수는 *개, 평균은 *" 라고 출력
+		
+		Random rd = new Random();
+		int size = rd.nextInt(100) + 1;
+		int[] worker = new int [size] ;
+		
+		// 값 설정하기 
+		for(int i =0; i<worker.length; i++) {
+			worker[i] = rd.nextInt();	
+		}
+		
+		// 합계 구하기 
+		int sum = 0;
+		for(int i=0;i<worker.length; i++) {
+			sum += worker[i];
+		}
+		
+		u.p("worker 개수는 " + worker.length + "개, 평균은"
+				+ ((double)sum) / worker.length);
+		
+		// Enhanced For
+		String[] seasons = {"Spring", "Summer", "Fall", "Winter"};
+		
+		for(int i=0;i<seasons.length;i++) {
+			u.p("Season : " + seasons[i]);
+		}
+		
+		for(String season : seasons) {
+			u.p("Season " + season);
+			// if(season == "Spring") break;
+		}
+		
+		String[][] school = {{"A1", "A2", "A3"}, {"B1", "B2", "B3"}};
+		
+		for(String[] classes  : school) {
+			for(String student : classes) {
+				u.p("Studen : " + student);
+			}
+			
+		}
 
 	}
 
 }
+
+
+
+
+
